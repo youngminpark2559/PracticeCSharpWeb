d2f32540bccd929bbe7eb8a3573c263abfc734b3
youngmtool <youngmtool@gmail.com>
2017-10-27 15:05:09 +0900

    Add AtataSettings.cs in TestAtataClassLibrary

--- TestAtataClassLibrary/TestAtataClassLibrary/AtataSettings.cs
+++ TestAtataClassLibrary/TestAtataClassLibrary/AtataSettings.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Atata;
 
+[assembly: Culture("en-us")]
+[assembly: VerifyTitleSettings(Format = "{0} - Atata Sample App")]
 namespace TestAtataClassLibrary
 {
-    public class Class1
+    class AtataSettings
     {
     }
 }
