52bfefaf3275a66c38423d335f18b45f9bd41d2e
youngmtool <youngmtool@gmail.com>
2017-10-27 17:57:29 +0900

    Add AtataSampleAppTests

---
+++ src/AtataSampleApp.AutoTests.Components/NotFoundPage.cs
@@ -0,0 +1,13 @@
+﻿using Atata;
+
+namespace AtataSampleApp.AutoTests
+{
+    using _ = NotFoundPage;
+
+    [Name("Page Not Found")]
+    [VerifyTitle]
+    [VerifyH1(TermMatch.Contains)]
+    public class NotFoundPage : AppPage<_>
+    {
+    }
+}
---
+++ src/AtataSampleApp.AutoTests.Components/SettingsPage.cs
@@ -0,0 +1,12 @@
+﻿using Atata;
+
+namespace AtataSampleApp.AutoTests
+{
+    using _ = SettingsPage;
+
+    [VerifyTitle]
+    [VerifyContent("Should contain some settings")]
+    public class SettingsPage : AppPage<_>
+    {
+    }
+}
